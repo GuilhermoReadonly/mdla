440dae70f9a5e9356c08e58c32add4f32312b6cb
guilhermo <guilhermo.readonly@gmail.com>
2022-03-16 22:15:31 +0100

    change word list

--- mdla-server/utils/sanitize_words.py
+++ mdla-server/utils/sanitize_words.py
@@ -4,15 +4,65 @@ Use this script to generate a word list:
 > python utils/sanitize_words.py 
 """
 
-file_in = open("./utils/word_list_in")
-file_out = open("./word_list", "w")
+
+import unicodedata
+
+
+def strip_accents(s: str) -> str:
+    return "".join(
+        c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
+    )
+
+
+file_in = open("./mdla-server/utils/word_list_in")
+file_out = open("./word_list_all.db", "w")
 
 lines = file_in.readlines()
 
 words = []
 for (i, w) in enumerate(lines):
-    w = w[1:-3]
-    words.append(w)
+
+    if (
+        ("α" in w)
+        or ("β" in w)
+        or ("γ" in w)
+        or ("δ" in w)
+        or ("ε" in w)
+        or ("ζ" in w)
+        or ("η" in w)
+        or ("θ" in w)
+        or ("κ" in w)
+        or ("λ" in w)
+        or ("μ" in w)
+        or ("ξ" in w)
+        or ("ο" in w)
+        or ("π" in w)
+        or ("ρ" in w)
+        or ("σ" in w)
+        or ("τ" in w)
+        or ("υ" in w)
+        or ("φ" in w)
+        or ("χ" in w)
+        or ("ψ" in w)
+        or ("ω" in w)
+        or ("ℓ" in w)
+        or ("µ" in w)
+        or ("ι" in w)
+        or ("ν" in w)
+    ):
+        continue
+
+    w_normalized = strip_accents(w.strip())
+    w_normalized = w_normalized.replace("œ", "oe")
+    w_normalized = w_normalized.replace("æ", "ae")
+
+    w_normalized = w_normalized.upper()
+
+    words.append(w_normalized)
+
+words = list(set(words))
+
+words.sort()
 
 file_out.write("\n".join(words))
 
